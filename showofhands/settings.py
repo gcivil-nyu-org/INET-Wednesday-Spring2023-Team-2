6456169a804d7c59b1e2b6dce7f7bb10d1e597b8
xiaolin-zhong <xz3343@nyu.edu>
2023-02-26 03:59:26 -0500

    added static: css & js

--- showofhands/settings.py
+++ showofhands/settings.py
@@ -14,13 +14,14 @@ from pathlib import Path
 import os
 # import environ
 from dotenv import load_dotenv
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+	
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -122,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
@@ -131,8 +132,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_DIRS = (
+    PROJECT_ROOT + '/static/',
+)
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- showofhands/urls.py
+++ showofhands/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 
 
@@ -25,3 +27,5 @@ urlpatterns = [
     path('posts/', include('posts.urls')),
     path('account/', include('login.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
