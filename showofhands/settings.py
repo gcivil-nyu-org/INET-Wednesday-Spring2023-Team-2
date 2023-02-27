0f589326c44c3fc6518806c8d8046abe341697f8
Hari <hnnhariharan12@gmail.com>
2023-02-27 15:27:59 -0500

    reordered static folder, modified loc in settings

--- showofhands/settings.py
+++ showofhands/settings.py
@@ -134,11 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_ROOT = ''
 STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATICFILES_DIRS = (
-    PROJECT_ROOT + '/static/',
+    str(BASE_DIR) + '/static/',
 )
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
