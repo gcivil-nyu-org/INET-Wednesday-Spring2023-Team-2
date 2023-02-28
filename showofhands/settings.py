7760983e7c02f74d4809f0b3aa70cc114202a341
Hari <hnnhariharan12@gmail.com>
2023-02-28 16:03:16 -0500

    added postgres db for local

--- showofhands/settings.py
+++ showofhands/settings.py
@@ -81,10 +81,23 @@ WSGI_APPLICATION = 'showofhands.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#sqlite3
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#postgres
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB_NAME'), 
+        'USER': os.getenv('POSTGRES_USER_NAME'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'), 
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
