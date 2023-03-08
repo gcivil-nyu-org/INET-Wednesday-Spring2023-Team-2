4c1c1658ff83e5a5d3f1830f66f1ad1e65944681
Hari <hnnhariharan12@gmail.com>
2023-03-08 12:40:19 -0500

    updated post model

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,21 @@
 from django.db import models
+from login.models import Custom_User
 
 # Create your models here.
 
 
 class Post_Model(models.Model):
-    pass
+    question_text = models.CharField(max_length=200)
+    created_by = models.ForeignKey(Custom_User, related_name='posts_created')
+
+    def __str__(self):
+        return self.question_text
+    
+
+class Options_Model(models.Model):
+    question = models.ForeignKey(Post_Model, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
