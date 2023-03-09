9daf0920dba007716f0c1e8774854dace327c6c2
Hari <hnnhariharan12@gmail.com>
2023-03-09 12:16:00 -0500

    added ajax functions with views function for content based refresh

--- posts/urls.py
+++ posts/urls.py
@@ -7,5 +7,5 @@ app_name = "posts"
 urlpatterns = [
     path("home/", views.home_view, name="home_page"),
     path("<int:pid>/", views.posts_view, name="post_generation_page"),
-    path("test1/", views.test1_view, name="test1")
+    path("<int:pid>/currpost/", views.show_curr_post_api_view, name="show_curr_post_api")
 ]
