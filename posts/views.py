cb1fa04e31b705dfb89f70a8d844028dcebd4816
Elise Siino <es5712@nyu.edu>
2023-03-11 13:34:10 -0500

    restarting home page by taking contents from profile

--- posts/views.py
+++ posts/views.py
@@ -116,7 +116,7 @@ def home_view(request):
         return results_view(request, pid)
 
     contents = {"post": post_, "options": options_, "display_result": False}
-    return render(request, "pages/post_home.html", contents)
+    return render(request, "pages/posts_home.html", contents)
 
     # template = loader.get_template("pages/poll_disp.html")
     # post_ = Post_Model.objects.get(pk=pid)
@@ -185,8 +185,7 @@ class PostsView(View):
 
         if call == "noapi":
             contents = {"post": post_, "options": options_, 'pid': pid}
-            # print(contents)
-            return render(request, "pages/post_home.html", contents)
+            return render(request, "pages/posts_home.html", contents)
         
         if post_.viewed_by.filter(username=request.user.username).exists():
             #display results
