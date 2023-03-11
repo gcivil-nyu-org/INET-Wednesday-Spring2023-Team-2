04cbf103c34375c2978cdf44574d28c25b8069e4
Hari <hnnhariharan12@gmail.com>
2023-03-10 19:57:44 -0500

    update to make ajax work, need to fix url not changing

--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,9 @@ from django.urls import reverse
 from django.contrib import messages
 from django.http import JsonResponse
 from django.template import loader
+from django.views import View
+
+
 import random
 
 
@@ -15,17 +18,17 @@ from login.models import Custom_User
 
 
 
-def is_ajax(request):
-    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
-
 #make an api return func to give polls and once next or home ot polls is clicked, ajax calls this func to get the next poll
 
 # Create your views here.
 
 
-# home page - will generate random post id that user hasn't interacted with to display for user - will change to empty later in urls
-# generate id and redirect/reverse with that parameter
-def home_view(request):
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+
+
+
+def get_random_pid():
     pids = Post_Model.objects.all()
 
     ##to check if user has alread seen/ interaacted with the post
@@ -37,9 +40,24 @@ def home_view(request):
         pid = random.choice(list(pids))
         pid = pid.pk
 
-        return redirect(reverse("posts:post_generation_page", kwargs={"pid": pid}))
+        print(pid, list(pids))
+
+        return (pid, True)
 
     except:
+        return (None, False)
+
+
+
+
+# home page - will generate random post id that user hasn't interacted with to display for user - will change to empty later in urls
+# generate id and redirect/reverse with that parameter
+def home_view(request):
+    pid, truth = get_random_pid()
+
+    if truth:
+        return redirect(reverse("posts:post_generation_page", kwargs={"pid": pid}))
+    else:
         return render(request, "pages/home.html")
 
 
@@ -127,61 +145,123 @@ def results_view(request, pid):
 
 
 def show_curr_post_api_view(request, pid):
-    return posts_view(request, pid, call="api")
+    print("kk")
+    post_view_class = PostsView()
+    if request.method == 'GET':
+        return post_view_class.get(request=request, pid=pid, call="api")
+    return post_view_class.post(request=request, pid=pid, call="api")
     
 
 
+#put ajax in poll_disp.html
 
+class PostsView(View):
+    def get(self, request, pid, call="noapi"):
+        post_ = Post_Model.objects.get(pk=pid)
+        options_ = post_.options_model_set.all()
 
-
-
-def posts_view(request, pid, call="noapi"):
-    post_ = Post_Model.objects.get(pk=pid)
-    options_ = post_.options_model_set.all()
-
-    ##to directly show results if user has already voted!
-    ## Remove later as user should not even see posts that has already been interacted with
-    # if post_.viewed_by.filter(username=request.user.username).exists():
-    #     #display results
-    #     return results_view(request, pid)
-
-    if request.method == "POST" and is_ajax(request):
-        try:
-            selected_choice = post_.options_model_set.get(pk=request.POST["option"])
-        except (KeyError, Options_Model.DoesNotExist):
-            print("error")
-            messages.error(request, "Select an option to submit!")
-            return
-            
-            # template = loader.get_template("pages/poll_disp.html")
-            # post_ = Post_Model.objects.get(pk=pid)
-            # options_ = post_.options_model_set.all()
-            # contents = {"post": post_, "options": options_, 'pid': pid}
-            # # return HttpResponse(template.render(contents, request))
-            # return JsonResponse({'voting': 'Failed!'})
-            
-
-        selected_choice.votes += 1
-        selected_choice.chosen_by.add(request.user)
-        selected_choice.save()
-
-        post_.viewed_by.add(request.user)
-        post_.save()
-
-        # display results
-        # return results_api_view(request, pid, call="api")
-        return JsonResponse({'voting': 'success'})
-    
-    elif request.method == 'POST':
-        return
-    
-    if call == "noapi":
+        if call == "noapi":
+            contents = {"post": post_, "options": options_, 'pid': pid}
+            return render(request, "pages/post_home.html", contents)
+        
+        if post_.viewed_by.filter(username=request.user.username).exists():
+            #display results
+            return results_view(request, pid)
+        
+        template = loader.get_template("pages/poll_disp.html")
         contents = {"post": post_, "options": options_, 'pid': pid}
-        return render(request, "pages/post_home.html", contents)
+        return HttpResponse(template.render(contents, request))
     
-    template = loader.get_template("pages/poll_disp.html")
-    contents = {"post": post_, "options": options_, 'pid': pid}
-    return HttpResponse(template.render(contents, request))
+    def post(self, request, pid, call="noapi"):
+        if is_ajax(request):
+            post_ = Post_Model.objects.get(pk=pid)
+            try:
+                selected_choice = post_.options_model_set.get(pk=request.POST["option"])
+            except (KeyError, Options_Model.DoesNotExist):
+                print("error")
+                messages.error(request, "Select an option to submit!")
+                return JsonResponse({'voting': 'Wrong request, nope'}) 
+                
+
+            selected_choice.votes += 1
+            selected_choice.chosen_by.add(request.user)
+            selected_choice.save()
+
+            post_.viewed_by.add(request.user)
+            post_.save()
+            return JsonResponse({'voting': 'success'})
+        return JsonResponse({'voting': 'Wrong request'})
 
 
 
+
+
+
+
+# def posts_view(request, pid, call="noapi"):
+#     post_ = Post_Model.objects.get(pk=pid)
+#     options_ = post_.options_model_set.all()
+
+#     ##to directly show results if user has already voted!
+#     ## Remove later as user should not even see posts that has already been interacted with
+#     # if post_.viewed_by.filter(username=request.user.username).exists():
+#     #     #display results
+#     #     return results_view(request, pid)
+
+#     if request.method == "POST" and is_ajax(request):
+#         try:
+#             selected_choice = post_.options_model_set.get(pk=request.POST["option"])
+#         except (KeyError, Options_Model.DoesNotExist):
+#             print("error")
+#             messages.error(request, "Select an option to submit!")
+#             return
+            
+#             # template = loader.get_template("pages/poll_disp.html")
+#             # post_ = Post_Model.objects.get(pk=pid)
+#             # options_ = post_.options_model_set.all()
+#             # contents = {"post": post_, "options": options_, 'pid': pid}
+#             # # return HttpResponse(template.render(contents, request))
+#             # return JsonResponse({'voting': 'Failed!'})
+            
+
+#         selected_choice.votes += 1
+#         selected_choice.chosen_by.add(request.user)
+#         selected_choice.save()
+
+#         post_.viewed_by.add(request.user)
+#         post_.save()
+
+#         # display results
+#         # return results_api_view(request, pid, call="api")
+#         return JsonResponse({'voting': 'success'})
+    
+#     elif request.method == 'POST':
+#         return JsonResponse({'voting': 'success'})
+    
+#     if call == "noapi":
+#         print("Hello")
+#         contents = {"post": post_, "options": options_, 'pid': pid}
+#         return render(request, "pages/post_home.html", contents)
+    
+#     template = loader.get_template("pages/poll_disp.html")
+#     contents = {"post": post_, "options": options_, 'pid': pid}
+#     return HttpResponse(template.render(contents, request))
+
+
+
+
+
+
+
+def show_next_post_api_view(request):
+    pid, truth = get_random_pid()
+
+    if truth:
+        post_view_class = PostsView()
+        if request.method == 'GET':
+            return post_view_class.get(request=request, pid=pid, call="api")
+        return post_view_class.post(request=request, pid=pid, call="api")
+    
+    else:
+        ## need to implement an empty template to say you have reached the end! and pass a httpresponse here
+        pass
