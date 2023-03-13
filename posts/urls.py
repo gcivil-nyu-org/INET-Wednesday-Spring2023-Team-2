911e3b7f969ef326ddbcb152cb4c570a3af6a8c4
Hari <hnnhariharan12@gmail.com>
2023-03-12 20:53:58 -0400

    added comment system backend

--- posts/urls.py
+++ posts/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("show_curr_post/", views.show_curr_post_api_view, name="show_curr_post_api"),
     path("show_next_post/", views.show_next_post_api_view, name="show_next_post_api"),
     path("get_current_url/", views.CurrentPostURL.as_view(), name="get_current_post_url_api"),
-    path("show_comments/", views.show_comments_api_view, name="show_comments_api"),
+    path("show_comments_text/", views.show_comments_text_api, name="show_comments_text_api"),
+    path("show_comments/", views.CommentsView.as_view(), name="show_comments_api"),
     # path("get_current_url/", views.get_current_url_api_view, name="get_current_url_api")
 ]
