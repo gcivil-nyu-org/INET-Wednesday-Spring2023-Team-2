4c13cda747e5c736c49805fa798aa6bbad6f5b4e
Hari <hnnhariharan12@gmail.com>
2023-03-16 16:58:41 -0400

    modified urls, views to access individual profiles

--- login/urls.py
+++ login/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
         views.password_reset_view,
         name="passwordreset_page",
     ),
-    path("profile/<username_>", views.profile_view, name="profile_page"),
+    path("profile/<str:username_>/", views.profile_view, name="profile_page"),
     # re_path("^profile/(?:name=(?P<username_>\w+))/$", views.UserHistory.as_view(), name="profile_page"),
     # re_path("profile/", login_required(views.UserHistory.as_view()), name="profile_page"),
 
