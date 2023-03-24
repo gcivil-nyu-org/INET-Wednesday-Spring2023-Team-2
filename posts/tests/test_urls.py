dbd6aa2daa9101b47350018ec56694e042b8e618
Hari <hnnhariharan12@gmail.com>
2023-03-24 10:15:17 -0400

    remove ajax from poll_create, added simple tests for home, poll_create pages

--- posts/tests/test_urls.py
+++ posts/tests/test_urls.py
@@ -1,9 +1,58 @@
-# from django.test import SimpleTestCase
-# from django.urls import reverse, resolve
-# from posts.views import PostsView, show_curr_post_api_view, home_view, results_view
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse, resolve
+from posts.views import PostsView, show_curr_post_api_view, home_view, results_view
+from posts.models import Post_Model, Options_Model, Comments_Model, UserPostViewTime
+from login.models import Custom_User
+from django.test import Client
 
 
-# class TestURLS(SimpleTestCase):
-#     def test_homepage_url_is_resolved(self):
-#         url = reverse("home_page")
-#         print(resolve(url))
+#302: Redirect
+#200: OK (Rendering HTML)
+#301: 
+
+
+class HomePageTest(TestCase):
+    def test_homepage_url(self):
+        user1 = Custom_User.objects.create(username='test1', password='test123')
+        post1 = Post_Model.objects.create(question_text="hi", created_by=user1)
+        # option1 = Options_Model.objects.create(question=post1, choice_text='option1')
+        # option2 = Options_Model.objects.create(question=post1, choice_text='option2')
+
+        response = self.client.get(reverse("posts:home_page"))
+        # print(response.status_code)
+        self.assertEqual(response.status_code, 302)
+
+
+
+class PostGenerationTest(TestCase):
+    def test_post_generation_page_url(self):
+        user1 = Custom_User.objects.create(username='test2', password='test1234')
+        post1 = Post_Model.objects.create(question_text="hi2", created_by=user1)
+        # option1 = Options_Model.objects.create(question=post1, choice_text='option1')
+        # option2 = Options_Model.objects.create(question=post1, choice_text='option2')
+
+        response = self.client.get(reverse("posts:post_generation_page", kwargs={'pid':2}))
+        # print(response.status_code)
+        self.assertEqual(response.status_code, 200)
+
+
+
+class CreatePollTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = Custom_User.objects.create_user('test3', 'test1234')
+        self.client.login(username='test3', password='test1234')
+
+    # def testLogin(self):
+    #     self.client.login(username='test3', password='test1234')
+    #     response = self.client.get(reverse('account:login_page'))
+    #     self.assertEqual(response.status_code, 200)
+
+    def test_create_poll_url(self):
+        # self.client.login(username='test3', password='test1234')
+        response1 = self.client.post(reverse("account:login_page"), {'username': 'test3', 'password': 'test1234', 'access_info': 'Sign In'})
+        print(response1)
+        response = self.client.get(reverse('posts:create_poll'))
+        print("create_poll:", response.status_code)
+        self.assertEqual(response.status_code, 302)
+        
