9ddb8f212b6d634c4b61a30fe156736dbc0b8ae7
Hari <hnnhariharan12@gmail.com>
2023-03-24 19:39:56 -0400

    modified posts model test

--- posts/tests/test_models.py
+++ posts/tests/test_models.py
@@ -15,13 +15,13 @@ from posts.models import Post_Model, Options_Model, Comments_Model, UserPostView
 class ModelTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.user = Custom_User.objects.create_user("test4", "test1234")
+        self.user = Custom_User.objects.create_user(username = "test4", password = "test1234", id = 1)
         self.client.login(username="test4", password="test1234")
-        self.model1_param = {"question_text": "q1", "created_by": self.user}
+        self.model1_param = {"question_text": "q1", "created_by": self.user, "id": 1}
         self.model1 = Post_Model.objects.create(**self.model1_param)
         self.model1.save()
 
-        self.option1_param = {"question": self.model1, "choice_text": "op1"}
+        self.option1_param = {"question": self.model1, "choice_text": "op1", "id": 1}
         self.option1 = Options_Model.objects.create(**self.option1_param)
         self.option1.save()
 
@@ -29,11 +29,12 @@ class ModelTest(TestCase):
             "question": self.model1,
             "comment_text": "hello",
             "commented_by": self.user,
+            "id": 1
         }
         self.comment1 = Comments_Model.objects.create(**self.comment1_param)
         self.comment1.save()
 
-        self.user_post_view_time1_param = {"user": self.user, "post": self.model1}
+        self.user_post_view_time1_param = {"user": self.user, "post": self.model1, "id": 1}
         self.user_post_view_time1 = UserPostViewTime.objects.create(
             **self.user_post_view_time1_param
         )
@@ -43,12 +44,14 @@ class ModelTest(TestCase):
         self.assertEqual(self.model1.question_text, self.model1_param["question_text"])
         self.assertEqual(self.model1.created_by, self.model1_param["created_by"])
         self.assertEqual(self.model1.category, "misc")
+        self.assertEqual(self.model1.__str__(), f"{self.model1_param['id']} => {self.model1_param['question_text']}")
 
     def test_option_created(self):
         self.assertEqual(self.option1.question, self.option1_param["question"])
         self.assertEqual(self.option1.choice_text, self.option1_param["choice_text"])
         self.assertEqual(self.option1.votes, 0)
         self.assertEqual(self.option1.color, "AED9E0")
+        self.assertEqual(self.option1.__str__(), f"{self.model1.__str__()} : {self.option1_param['choice_text']}")
 
     def test_comment_created(self):
         self.assertEqual(self.comment1.question, self.comment1_param["question"])
@@ -58,6 +61,7 @@ class ModelTest(TestCase):
         self.assertEqual(
             self.comment1.commented_by, self.comment1_param["commented_by"]
         )
+        self.assertEqual(self.comment1.__str__(), f"{self.model1.__str__()} => {self.comment1_param['commented_by'].__str__()} : {self.comment1_param['comment_text']}")
 
     def user_post_view_time_created(self):
         self.assertEqual(
