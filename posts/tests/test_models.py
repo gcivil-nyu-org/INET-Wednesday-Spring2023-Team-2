6c031653bdfb1a8e597499d9872cdac44143378f
Hari <hnnhariharan12@gmail.com>
2023-03-24 17:18:58 -0400

    modified post tests

---
+++ posts/tests/test_models.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse, resolve
+from posts.views import PostsView, show_curr_post_api_view, home_view, results_view
+from posts.models import Post_Model, Options_Model, Comments_Model, UserPostViewTime
+from login.models import Custom_User
+from django.test import Client
+
+
+#302: Redirect
+#200: OK (Rendering HTML)
+#301: 
+
+
