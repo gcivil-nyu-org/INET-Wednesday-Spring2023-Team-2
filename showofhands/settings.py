9352d3b7622c785d96ae2c44773498ea95e20cfd
Hari <hnnhariharan12@gmail.com>
2023-03-29 23:36:17 -0400

    black

--- showofhands/settings.py
+++ showofhands/settings.py
@@ -230,9 +230,9 @@ USE_TZ = False
 STORAGES = {"default": "my_django_app.custom_storage.MediaStorage"}
 
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')  
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')  
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 # AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
