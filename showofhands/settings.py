5752fc2c5bc3b3b253be4dce1c196f59869d8a25
Hari <hnnhariharan12@gmail.com>
2023-03-30 20:23:49 -0400

    black

--- showofhands/settings.py
+++ showofhands/settings.py
@@ -201,7 +201,6 @@ LOGIN_URL = "/account/login"
 LOGOUT_REDIRECT_URL = "/home"
 
 
-
 # REST_FRAMEWORK = {
 #     # Use Django's standard `django.contrib.auth` permissions,
 #     # or allow read-only access for unauthenticated users.
@@ -234,7 +233,7 @@ else:
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (
     str(BASE_DIR) + "/login/static/",
