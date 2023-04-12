2bf1ea9d37161ac8101db3be1ae79cd02d0daa1f
Hari <hnnhariharan12@gmail.com>
2023-04-12 12:07:22 -0400

    changes in chat socket for redis

--- showofhands/settings.py
+++ showofhands/settings.py
@@ -259,7 +259,7 @@ if "USE_REDIS_ENDPOINT" in os.environ and os.environ["USE_REDIS_ENDPOINT"]:
             "BACKEND": "channels_redis.core.RedisChannelLayer",
             "CONFIG": {
                 "hosts": [
-                    ("aws-my-1sw7m30jtg0i4.yxaols.0001.use1.cache.amazonaws.com", 6379)
+                    ("aws-my-976w8azpc9xc.yxaols.0001.use1.cache.amazonaws.com", 6379)
                 ],
             },
         },
