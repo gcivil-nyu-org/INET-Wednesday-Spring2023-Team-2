7abe25813986c4b2bdcf951090df86effe55879c
egger <egger4869@gmail.com>
2023-05-01 19:23:24 -0400

    varies css fix, notification shows comment text

--- posts/views.py
+++ posts/views.py
@@ -561,6 +561,7 @@ class CommentsView(View):
                     noti_post = Noti_Model.objects.create(
                         recipient=post_.created_by,
                         sender=request.user,
+                        content_text=comment_text,
                         post_at=post_,
                         noti_type="Comment",
                     )
@@ -584,6 +585,7 @@ class CommentsView(View):
                             noti_at = Noti_Model.objects.create(
                                 recipient=target,
                                 sender=request.user,
+                                content_text=comment_text,
                                 post_at=post_,
                                 noti_type="At",
                             )
