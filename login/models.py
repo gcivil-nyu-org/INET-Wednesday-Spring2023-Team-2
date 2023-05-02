f16cac046d4ab994df7e08dab6c33afe484de868
Hari <hnnhariharan12@gmail.com>
2023-05-02 17:17:29 -0400

    modified username to disallow symbols except '_'

--- login/models.py
+++ login/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
+from django.core import validators
+from django.utils.translation import gettext_lazy as _
 
 import os
 
@@ -32,7 +34,32 @@ class Custom_User(AbstractUser):
         validators=[validate_image_extension],
     )
 
+    username = models.CharField(
+        _("username"),
+        max_length=30,
+        unique=True,
+        help_text=_(
+            "Required. 30 characters or fewer. Letters, digits and " "@/./+/-/_ only."
+        ),
+        validators=[
+            validators.RegexValidator(
+                r"^[\w]+$",
+                _(
+                    "Enter a valid username. "
+                    "This value may contain only letters, numbers "
+                    "and _ characters."
+                ),
+                "invalid",
+            ),
+        ],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
+
     has_unread_messages = models.BooleanField(blank=True, null=True)
+
+    USERNAME_FIELD = "username"
 
     def __str__(self):
         return self.username
